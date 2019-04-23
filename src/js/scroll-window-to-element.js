8864b8b357fe23c29caee9e89725e48fd2abb150
Vadim <pv@petrovvadim.com>
2019-04-23 09:19:26 +0300

    refactored es5 version

--- src/js/scroll-window-to-element.js
+++ src/js/scroll-window-to-element.js
@@ -1,13 +1,4 @@
-// Animation function with requestAnimationFrame()
-function animate(draw, duration) {
-	const start = performance.now();
-	requestAnimationFrame(function _animate(time) {
-		let timePassed = time - start;
-		if (timePassed > duration) timePassed = duration;
-		draw(timePassed);
-		if (timePassed < duration) requestAnimationFrame(_animate);
-	});
-};
+'use strict';
 
 function ScrollToElement(opts) {
 	const defaults = {
@@ -19,64 +10,64 @@ function ScrollToElement(opts) {
 		init: true,
 	};
 	this.opts = Object.assign(defaults, (opts || {}));
+
+	// Animation function with requestAnimationFrame()
+	this.animate = function (draw, duration) {
+		const start = performance.now();
+		requestAnimationFrame(function _animate(time) {
+			let timePassed = time - start;
+			if (timePassed > duration) timePassed = duration;
+			draw(timePassed);
+			if (timePassed < duration) requestAnimationFrame(_animate);
+		});
+	};
+
+	this.init = function () {
+		const that = this;
+		const anchors = document.querySelectorAll(this.opts.anchors);
+		const anchorsArr = [].slice.call(anchors);
+		anchorsArr.forEach(function (a, i) {
+			a.addEventListener('click', function (e) { that.clickHandler(e) });
+		});
+	}
+
+	this.makeEaseInOut = function (timing, progress) {
+		return function () {
+			if (progress < 0.5) {
+				return timing(2 * progress) / 2;
+			} else { return (2 - timing(2 * (1 - progress))) / 2; }
+		};
+	}
+
+	this.timingFn = function (progress, startVal, displace) {
+		if (this.easing === 'linear') {
+			return (progress * displace) + startVal;
+		} else {
+			const easeInOut = this.makeEaseInOut(function (prgrs) { return Math.pow(prgrs, 2); }, progress);
+			return (easeInOut() * displace) + startVal;
+		}
+	}
+
+	this.clickHandler = function (e) {
+		if (e) { e.preventDefault(); }
+
+		const that = this,
+			link = e.currentTarget.getAttribute('href'),
+			elToScroll = document.querySelector(link) || document.querySelector('*[data-' + this.opts.dataAttrName + '="' + link.substr(1) + '"]'),
+			offsetTop = elToScroll.getBoundingClientRect().top + window.pageYOffset,
+			startVal = window.pageYOffset,
+			displace = offsetTop + this.opts.offset - startVal,
+			duration = this.opts.duration;
+
+		if (displace === 0) return;
+
+		that.animate(function (timePassed) {
+			let progress = timePassed / duration,
+				newVal = that.timingFn(progress, startVal, displace);
+			window.scrollTo(null, newVal);
+		}, duration);
+
+	}
+
 	if (this.opts.init) this.init();
 }
-
-ScrollToElement.prototype.init = function() {
-	const that = this;
-	const anchors = document.querySelectorAll(this.opts.anchors);
-	const anchorsArr = [].prototype.slice.call(anchors);
-	anchorsArr.forEach(function(a, i) {
-		a.addEventListener('click', function(e) { that.clickHandler(e) });
-	});
-}
-
-ScrollToElement.prototype.makeEaseInOut = function(timing, progress) {
-	return function() {
-		if (progress < 0.5) { return timing(2 * progress) / 2;
-		} else { return (2 - timing(2 * (1 - progress))) / 2; }
-	};
-}
-
-ScrollToElement.prototype.timingFn = function(progress, startVal, displace) {
-	if (this.easing === 'linear') {
-		return (progress * displace) + startVal;
-	} else {
-		const easeInOut = this.makeEaseInOut(function (prgrs) { return Math.pow(prgrs, 2); }, progress);
-		return (easeInOut() * displace) + startVal;
-	}
-}
-
-ScrollToElement.prototype.clickHandler = function(e) {
-	if (e) { e.preventDefault(); }
-
-	const that = this,
-		link = e.currentTarget.getAttribute('href'),
-		elToScroll = document.querySelector(link) || document.querySelector('*[data-' + this.opts.dataAttrName + '="' + link.substr(1) + '"]'),
-		offsetTop = this.elOffsetTop(elToScroll);
-
-	const startVal = window.pageYOffset,
-		endVal = offsetTop + this.opts.offset,
-		displace = endVal - startVal,
-		duration = this.opts.duration;
-
-	if (displace === 0) return;
-
-	animate(function(timePassed) {
-		let progress = timePassed / duration,
-			newVal = that.timingFn(progress, startVal, displace);
-		window.scrollTo(null, newVal);
-	}, duration);
-
-}
-
-ScrollToElement.prototype.elOffsetTop = function(el) {
-	let top = 0;
-	while (el.parentNode) {
-		top += el.offsetTop;
-		el = el.parentNode;
-	}
-	return top;
-}
-
-module.exports = scrollToElement;
